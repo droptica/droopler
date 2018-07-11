423dd54778aa9c20b669307ff5906082aa2031c5
gorsky87 <gp@gorskipawel.pl>
2018-07-11 18:47:10 +0200

    Issue: #34879

--- modules/custom/d_p_help/src/Controller/SupportController.php
+++ modules/custom/d_p_help/src/Controller/SupportController.php
@@ -10,19 +10,21 @@ use Drupal\Core\Controller\ControllerBase;
 class SupportController extends ControllerBase {
 
   /**
-   * Prints a page listing various types of help.
-   *
-   * The page has sections defined by \Drupal\help\HelpSectionPluginInterface
-   * plugins.
+   * Prints a page about Droopler distribution.
    *
    * @return array
-   *   A render array for the help page.
+   *   A render array for the info page.
    */
   public function render() {
-
+    $output = '<h3>' . t('Droopler') . '</h3>';
+    $output .= '<p>' . t('More info about Droopler - <a href=":link">See official Droopler website</a>.', [':link' => 'https://droopler.com/']) . '</p>';
+    $output .= '<h3>' . t('Support') . '</h3>';
+    $output .= '<p>' . t('Do You need support with Droopler? -  <a href=":link">Droptica.com</a>.', [':link' => 'https://droptica.com']) . '</p>';
+    $output .= '<h3>' . t('Github') . '</h3>';
+    $output .= '<p>' . t('Droopler on Github -  <a href=":link">https://github.com/droptica/droopler_project</a>.', [':link' => ' https://github.com/droptica/droopler_project']) . '</p>';
     return [
       '#type' => 'markup',
-      '#markup' => '<div class="container">' . t('Hello World!') . '</div>',
+      '#markup' => '<div class="container">' . $output . '</div>',
     ];
   }
 }
