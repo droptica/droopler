0576bb9eb1a436ac34da14852dc5b393addfdec5
Grzegorz Pietrzak <grzegorz.pietrzak@droptica.pl>
2018-08-03 10:36:45 +0200

    DROOP-51 CR fixes

--- modules/custom/d_p_counters/js/d_p_counters.js
+++ modules/custom/d_p_counters/js/d_p_counters.js
@@ -3,26 +3,24 @@
 
   Drupal.behaviors.d_p_counters = {
     attach: function (context, settings) {
-      $(document).ready(function () {
-        var cnt = 0;
-        var options = {
-          useEasing: true,
-          useGrouping: true,
-          separator: ' ',
-          decimal: ',',
-        };
+      var cnt = 0;
+      var options = {
+        useEasing: true,
+        useGrouping: true,
+        separator: ' ',
+        decimal: ',',
+      };
 
-        // Find all counters.
-        $('.paragraph--type--d-p-group-of-counters .field--name-field-d-number').each(function(){
-          // Trigger if in viewport.
-          inViewport(this, function(el) {
-            var id = 'upcnt' + cnt++;
-            $(el).attr('id', id);
+      // Find all counters.
+      $('.paragraph--type--d-p-group-of-counters .field--name-field-d-number', context).each(function(){
+        // Trigger if in viewport.
+        inViewport(this, function(el) {
+          var id = 'upcnt' + cnt++;
+          $(el).attr('id', id);
 
-            // Count up.
-            var numAnim = new CountUp(id, 0, $(el).attr('data-count'), 0, 2, options);
-            numAnim.start();
-          });
+          // Count up.
+          var numAnim = new CountUp(id, 0, $(el).attr('data-count'), 0, 2, options);
+          numAnim.start();
         });
       });
     }
