db60319d39bc8936d554d7d65335abc075d6f6b5
Pawel Gorski <pawel.gorski@droptica.pl>
2018-10-18 10:54:33 +0200

    DROOP-7

--- modules/custom/d_p/d_p.api.php
+++ modules/custom/d_p/d_p.api.php
@@ -2,32 +2,17 @@
 
 /**
  * @file
- * Hooks and documentation related to paragraphs module.
+ * Hooks and documentation related to d_p module.
  */
 
 /**
- * Alter paragraphs widget.
+ * Alter paragraph_list used in admin droopler widget.
  *
- * @param array $widget_actions
- *   Array with actions and dropdown widget actions.
- * @param array $context
- *   An associative array containing the following key-value pairs:
- *   - form: The form structure to which widgets are being attached. This may be
- *     a full form structure, or a sub-element of a larger form.
- *   - widget: The widget plugin instance.
- *   - items: The field values, as a
- *     \Drupal\Core\Field\FieldItemListInterface object.
- *   - delta: The order of this item in the array of subelements (0, 1, 2, etc).
- *   - element: A form element array containing basic properties for the widget.
- *   - form_state: The current state of the form.
- *   - paragraphs_entity: the paragraphs entity for this widget. Might be
- *     unsaved, if we have just added a new item to the widget.
- *   - is_translating: Boolean if the widget is translating.
- *   - allow_reference_changes: Boolean if changes to structure are OK.
+ * @param array $paragraph_list
+ *   Paragraphs list with title and icon/image.
+ *
+ * @param $paragraphs_entity
+ *   Entity paragraph
  */
-function hook_d_p_placeholder_alter(array &$paragraph_list) {
+function hook_d_p_placeholder_alter(array &$paragraph_list, $paragraphs_entity) {
 }
-
-/**
- * @} End of "addtogroup hooks".
- */
