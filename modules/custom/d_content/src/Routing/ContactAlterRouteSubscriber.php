3df1688ea349bbf7ea3ace5cc071afe6122527ce
malgosia-swierczynska-droptica <malgorzata.swierczynskachlasciak@droptica.pl>
2019-02-19 12:31:39 +0100

    DROOP-236-improve-ContactAlterRouteSubscriber: handle uninstall contact module

--- modules/custom/d_content/src/Routing/ContactAlterRouteSubscriber.php
+++ modules/custom/d_content/src/Routing/ContactAlterRouteSubscriber.php
@@ -9,7 +9,6 @@ namespace Drupal\d_content\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Symfony\Component\Routing\RouteCollection;
-
 /**
  * Listens to the dynamic route events.
  */
@@ -19,8 +18,10 @@ class ContactAlterRouteSubscriber extends RouteSubscriberBase {
    * {@inheritdoc}
    */
   public function alterRoutes(RouteCollection $collection) {
-    // Remove the /contact route.
-    $collection->remove('contact.site_page');
-  }
+    // Remove the /user/{user}/contact route.
 
+    if ($route = $collection->get('entity.user.contact_form')) {
+      $collection->remove('entity.user.contact_form');
+    }
+  }
 }
