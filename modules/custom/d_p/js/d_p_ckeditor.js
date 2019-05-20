060a6cdd729be96c4362e7398c6807c17db4a6f9
malgosia-swierczynska-droptica <malgorzata.swierczynskachlasciak@droptica.pl>
2019-05-20 12:38:08 +0200

    DROOP-83-align-long-textu: reviewers comments applied

--- modules/custom/d_p/js/d_p_ckeditor.js
+++ modules/custom/d_p/js/d_p_ckeditor.js
@@ -4,6 +4,23 @@
     attach: function (context, settings) {
 
       d_p_ckeditor(true);
+      d_p_ckeditor_add_js(true);
+
+      function d_p_ckeditor_add_js(doc) {
+        var cssId = 'd_p_ckeditor';
+        if (!doc.getElementById(cssId)) {
+          // add css for cke_editable class in ckeditor
+          var head = doc.getElementsByTagName('head')[0];
+          var link = doc.createElement('link');
+          link.id = cssId;
+          link.rel = 'stylesheet';
+          link.type = 'text/css';
+          link.href = '/profiles/contrib/droopler/modules/custom/d_p/css/d_p_ckeditor.css';
+          head.appendChild(link);
+        }
+      }
+
+      var interval = window.setInterval(d_p_ckeditor, 500);
 
       function d_p_ckeditor(fade_in) {
         fade_in = (fade_in == true) ? 500 : 0;
@@ -14,24 +31,14 @@
               try {
                 // Check if it is text-blocks-paragraph
                 if (typeof CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].element.$.parentElement.offsetParent.children['geysir-modal-form'] !== 'undefined') {
-                    var docgeysir = CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document.$; // get CKE doc!
-                    var cssIdgeysir = 'd_p_ckeditor';
-                    if (!docgeysir.getElementById(cssIdgeysir)) {
-                      // add css for cke_editable class in ckeditor
-                      var head = docgeysir.getElementsByTagName('head')[0];
-                      var link = docgeysir.createElement('link');
-                      link.id = cssIdgeysir;
-                      link.rel = 'stylesheet';
-                      link.type = 'text/css';
-                      link.href = '/profiles/contrib/droopler/modules/custom/d_p/css/d_p_ckeditor.css';
-                      head.appendChild(link);
-                  }
+                  var doc = CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document.$; // get CKE doc!
+                  d_p_ckeditor_add_js(doc);
                 }
               } catch (err) {
                 // Error handling
               }
             }, 1300);
-        } else {
+          } else {
             // For editor paragraph check every instance of ckeditor
             for (var instanceName in CKEDITOR.instances) {
               setTimeout(function () {
@@ -40,17 +47,7 @@
                 // check if it is group of text-block from block-text-paragraph
                 if (!(element.contains('paragraph-type--d-p-single-text-block'))) {
                   var doc = CKEDITOR.instances[instanceName].document.$; // get CKE doc!
-                  var cssId = 'd_p_ckeditor';
-                  if (!doc.getElementById(cssId)) {
-                    // add css for cke_editable class in ckeditor
-                    var head = doc.getElementsByTagName('head')[0];
-                    var link = doc.createElement('link');
-                    link.id = cssId;
-                    link.rel = 'stylesheet';
-                    link.type = 'text/css';
-                    link.href = '/profiles/contrib/droopler/modules/custom/d_p/css/d_p_ckeditor.css';
-                    head.appendChild(link);
-                  }
+                  d_p_ckeditor_add_js(doc);
                 }
               }, 1300);
             }
