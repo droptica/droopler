a192de6c0bdc5da1509955a353171ebbe3a8dc58
Pawel Gorski <pawel.gorski@droptica.pl>
2019-07-04 18:48:02 +0200

    DrupalOrg

--- themes/custom/droopler_dorg_subtheme/theme-settings.php
+++ themes/custom/droopler_dorg_subtheme/theme-settings.php
@@ -17,11 +17,3 @@ use Drupal\Core\Url;
 function droopler_dorg_subtheme_form_system_theme_settings_alter(&$form, FormStateInterface $form_state, $form_id = NULL) {
 
 }
-
-
-/**
- * Implements hook_library_info_alter().
- */
-function droopler_dorg_subtheme_library_info_alter(&$libraries, $extension) {
-  $a=2;
-}
