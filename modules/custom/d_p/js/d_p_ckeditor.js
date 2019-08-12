049fdbc8123ae0c94ec3643f8f1834833b2f6e79
malgosia-swierczynska-droptica <malgorzata.swierczynskachlasciak@droptica.pl>
2019-08-12 18:41:06 +0200

    DD-53: added reset button to restore default color

--- modules/custom/d_p/js/d_p_ckeditor.js
+++ modules/custom/d_p/js/d_p_ckeditor.js
@@ -6,8 +6,9 @@
       d_p_ckeditor(true);
 
       function d_p_ckeditor_geysir() {
-        if (typeof CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].element.$.parentElement.offsetParent.children['geysir-modal-form'] !== 'undefined') {
+        if (typeof CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].element.$.parentElement.offsetParent.offsetParent.parentElement.offsetParent.children['geysir-modal-form'] !== 'undefined') {
           if (typeof CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document !== 'undefined') {
+            d_p_add_reset_color_button();
             var doc = CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document.$;
             d_p_ckeditor_add_js(doc);
           } else {
@@ -22,13 +23,35 @@
           // check if it is group of text-block from block-text-paragraph
           if (!(element.contains('paragraph-type--d-p-single-text-block'))) {
             if (typeof CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document !== 'undefined') {
-              var doc = CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document.$; //get CKE doc!
-              d_p_ckeditor_add_js(doc);
-            } else {
-              setTimeout(d_p_ckeditor_notgeysir, 500);
+                d_p_add_reset_color_button();
+                var doc = CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document.$; //get CKE doc!
+                d_p_ckeditor_add_js(doc);
+              } else {
+                setTimeout(d_p_ckeditor_notgeysir, 500);
+              }
+            }
+        }
+      }
+
+      function d_p_add_reset_color_button() {
+        if(document.getElementsByClassName('form-type-color').length !== 0) {
+          var colors = document.getElementsByClassName('form-type-color');
+          var button = document.createElement("button");
+          button.innerHTML = "Reset color";
+          button.id = "reset-button";
+          button.classList = "button js-form-submit form-submit btn btn-primary";
+          button.onclick = d_p_reset_color;
+          for (var i = 0; i < colors.length; i++) {
+            if(colors[i].getElementsByTagName('button').length === 0){
+              colors[i].appendChild(button);
             }
           }
         }
+      }
+
+      function d_p_reset_color(event) {
+        event.preventDefault();
+        event.target.previousElementSibling.value = "#000000";
       }
 
       function d_p_ckeditor_add_js(doc) {
