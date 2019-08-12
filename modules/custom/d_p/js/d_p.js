c05dcd48a7497d3b9571a46c993cdf726cadbefb
bartek-flp <filipiuk.bartek@gmail.com>
2019-08-12 15:30:01 +0200

    tex blocks

--- modules/custom/d_p/js/d_p.js
+++ modules/custom/d_p/js/d_p.js
@@ -45,4 +45,25 @@
       }
     }
   };
+
+  /**
+   * Changes for Side by Side paragraph.
+   *
+   * @type {{attach: Drupal.behaviors.d_p_side_by_side.attach}}
+   */
+  Drupal.behaviors.d_p_side_by_side = {
+    attach: function (context, settings) {
+      var container = $('.d-p-side-by-side .items');
+      container.find('.items-wrapper .list-item-wrapper').each(function (key, value) {
+        var $this = $(this);
+        var $background_url = $this.find('.user-image-background').data('background');
+        var $prop = {
+          'background': 'url(' + $background_url + ')'
+        };
+        $this.find('.user-image-background').css('background-image', 'url(' + $background_url + ')').addClass('background-properties');
+      });
+    }
+  };
+
+
 })(jQuery);
