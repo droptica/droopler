4d770e182c123871dc1505735c630f1b74a6611b
malgosia-swierczynska-droptica <malgorzata.swierczynskachlasciak@droptica.pl>
2019-08-14 13:28:51 +0200

    DD-53: reviewer's comments applied

--- modules/custom/d_p/js/d_p_ckeditor.js
+++ modules/custom/d_p/js/d_p_ckeditor.js
@@ -24,34 +24,36 @@
           if (!(element.contains('paragraph-type--d-p-single-text-block'))) {
             if (typeof CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document !== 'undefined') {
                 d_p_add_reset_color_button();
-                var doc = CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document.$; //get CKE doc!
-                d_p_ckeditor_add_js(doc);
-              } else {
-                setTimeout(d_p_ckeditor_notgeysir, 500);
-              }
+              var doc = CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document.$; //get CKE doc!
+              d_p_ckeditor_add_js(doc);
+            } else {
+              setTimeout(d_p_ckeditor_notgeysir, 500);
             }
+          }
         }
       }
 
+      //Adding button next to choosing "Background color" to enable resetting to default color
       function d_p_add_reset_color_button() {
-        if(document.getElementsByClassName('form-type-color').length !== 0) {
+        if (document.getElementsByClassName('form-type-color').length !== 0) {
           var colors = document.getElementsByClassName('form-type-color');
-          var button = document.createElement("button");
-          button.innerHTML = "Reset color";
-          button.id = "reset-button";
-          button.classList = "button js-form-submit form-submit btn btn-primary";
+          var button = document.createElement('button');
+          button.innerHTML = Drupal.t('Reset color');
+          button.id = 'reset-button';
+          button.classList = 'button js-form-submit form-submit btn btn-primary';
           button.onclick = d_p_reset_color;
           for (var i = 0; i < colors.length; i++) {
-            if(colors[i].getElementsByTagName('button').length === 0){
+            if (colors[i].getElementsByTagName('button').length === 0) {
               colors[i].appendChild(button);
             }
           }
         }
       }
 
+      //Onclick 'reset-button' action that restores default value of background color
       function d_p_reset_color(event) {
         event.preventDefault();
-        event.target.previousElementSibling.value = "#000000";
+        event.target.previousElementSibling.value = '#000000';
       }
 
       function d_p_ckeditor_add_js(doc) {
