0645c7bf596c1a21486911996df342ed666e59db
Paweł Pietras <pawel.pietras@droptica.pl>
2019-08-26 13:48:15 +0200

    DD-55: js corrections

--- modules/custom/d_social_media/js/last-element-in-a-row.js
+++ modules/custom/d_social_media/js/last-element-in-a-row.js
@@ -1,9 +1,12 @@
+/**
+ * Style all far right elements in horizontal list.
+ */
 (function ($, Drupal) {
   Drupal.behaviors.socialMedia = {
     attach: function (context, settings) {
       var lastElement = false;
       var els = document.querySelectorAll('.social-media-wrapper ul li');
-      for(var i = 0; i < els.length; i++){
+      for (var i = 0; i < els.length; i++) {
         var el = els[i]; // current element
         if (lastElement && lastElement.offsetTop !== el.offsetTop) {
           lastElement.className += " last-element";
