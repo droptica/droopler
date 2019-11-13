26da016f266b4544590fb4433c6b7d2d0da06a84
Rafał Sereda <rafal.sereda@droptica.pl>
2019-11-13 22:51:12 +0100

    DD-85 code simplify - no logo reset when d_demo unistall needed

--- modules/custom/d_demo/src/Service/ThemeLogoService.php
+++ modules/custom/d_demo/src/Service/ThemeLogoService.php
@@ -33,25 +33,9 @@ class ThemeLogoService {
    * Function helps set current theme new demo logo.
    */
   public function setThemeDemoLogo() {
-    $currentThemeSettings = $this->getCurrentThemeConfig();
-    if (!$currentThemeSettings->get('default_logo')) {
-      $previousLogoPath = $currentThemeSettings->get('logo_path');
-
-      $this->setOriginalLogoPath($previousLogoPath);
-    }
-
     $demoModulePath = $this->moduleHandler->getModule('d_demo')->getPath();
 
     $this->setCurrentThemeDemoLogo($demoModulePath . '/assets/demo-logo.svg');
-  }
-
-  /**
-   * Function helps restore original settings for current theme logo.
-   */
-  public function unsetThemeDemoLogo() {
-    $originalThemeLogoPath = $this->getOriginalLogoPath();
-    $this->setCurrentThemeDemoLogo($originalThemeLogoPath);
-    $this->clearStoredOriginalLogoPath();
   }
 
   /**
@@ -79,50 +63,16 @@ class ThemeLogoService {
   }
 
   /**
-   * Getter for stored path from d_demo settings.
-   *
-   * @return array|mixed|null
-   */
-  protected function getOriginalLogoPath() {
-    $demoSettings = $this->getConfig('d_demo.settings');
-    return $demoSettings->get('original_logo_path');
-  }
-
-  /**
-   * Setter for stored path in d_demo settings.
-   *
-   * @param $originalLogoPath
-   */
-  protected function setOriginalLogoPath($originalLogoPath) {
-    /** @var Drupal\Core\Config\Config $demoSettings */
-    $demoSettings = $this->getConfig('d_demo.settings');
-    $demoSettings->set('original_logo_path', $originalLogoPath);
-    $demoSettings->save();
-  }
-
-  /**
-   * Clears setting for logo path in d_demo settings.
-   */
-  protected function clearStoredOriginalLogoPath() {
-    $demoSettings = $this->getConfig('d_demo.settings');
-    $demoSettings->clear('original_logo_path');
-    $demoSettings->save();
-  }
-
-  /**
    * Helper for updating current theme settings for logo field.
    *
    * @param $logoPath
    */
   protected function setCurrentThemeDemoLogo($logoPath) {
-    $currentThemeSettings = $this->getCurrentThemeConfig();
     if ($logoPath) {
+      $currentThemeSettings = $this->getCurrentThemeConfig();
       $currentThemeSettings->set('logo.use_default', FALSE);
       $currentThemeSettings->set('logo.path', $logoPath );
-    } else {
-      $currentThemeSettings->set('logo.use_default', TRUE);
-      $currentThemeSettings->clear('logo.path' );
+      $currentThemeSettings->save();
     }
-    $currentThemeSettings->save();
   }
 }
