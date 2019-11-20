9fc0a92c9756c76c1c1625d49cac57ef5d6a054d
Rafał Sereda <rafal.sereda@droptica.pl>
2019-11-20 14:37:45 +0100

    DD-62 mobile menu sytling and js functionallity improving, work in progress

--- themes/custom/droopler_theme/js/main-menu.js
+++ themes/custom/droopler_theme/js/main-menu.js
@@ -11,24 +11,15 @@
       var $block = $('.we-mega-menu-submenu .block-block-content', context);
       var $titles = $('.field--name-field-d-main-title', $block);
 
-      function showHideBlockContents() {
-        window.innerWidth < breakpointDesktop ? $contents.hide() : $contents.show();
-        $contents.removeClass(visibleClass);
-      }
-
       if ($titles.length) {
         var breakpointDesktop = 992;
-        var visibleClass = '.visible-content';
         var blockContentClass = '.field--name-field-d-long-text';
-        var $contents = $titles.next(blockContentClass);
-        showHideBlockContents();
-        $(window).resize(showHideBlockContents);
 
         $titles.click(function() {
           if (window.innerWidth < breakpointDesktop) {
-            var $nextContent = $(this).next(blockContentClass);
-            $contents.not($nextContent).slideUp().removeClass(visibleClass);
-            $nextContent.not(visibleClass).slideDown().addClass(visibleClass);
+            $(this).toggleClass('open').parent().find(blockContentClass).slideToggle();
+
+            return false;
           }
         });
       }
@@ -38,7 +29,6 @@
   Drupal.behaviors.mainMenuMobileNavbarListener = {
     attach: function (context, settings) {
       $('#navbar-main button.navbar-toggler').click(function() {
-      console.log('asd');
         $('body').toggleClass('navbar-open');
       });
     }
@@ -54,8 +44,8 @@
 
         $links.each(function() {
           $(this).toggleClass('open', $(this).parent().is('.active'));
-        }).click(function() {
-          $(this).toggleClass('open').next(blockContentClass).slideToggle();
+        }).find('.d-submenu-toggler').click(function() {
+          $(this).parent().toggleClass('open').next(blockContentClass).find('> .we-mega-menu-submenu-inner').slideToggle();
 
           return false;
         });
