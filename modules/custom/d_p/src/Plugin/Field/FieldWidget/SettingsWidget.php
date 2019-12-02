635f240a8c2bc768c3daa1337e5da10f24a1a01e
Grzegorz Pietrzak <grzegorz.pietrzak@droptica.pl>
2019-12-02 16:26:41 +0100

    DD-142 New subfields in d_settings field.

--- modules/custom/d_p/src/Plugin/Field/FieldWidget/SettingsWidget.php
+++ modules/custom/d_p/src/Plugin/Field/FieldWidget/SettingsWidget.php
@@ -21,13 +21,65 @@ use Drupal\Core\Form\FormStateInterface;
 class SettingsWidget extends WidgetBase {
 
   const CSS_CLASS_SETTING_NAME = 'custom_class';
+  const HEADING_TYPE_SETTING_NAME = 'heading_type';
 
   private function getConfigOptions() {
     return [
       self::CSS_CLASS_SETTING_NAME => [
-        'title' => $this->t('Custom Class'),
+        'title' => $this->t('Additional classes for the paragraph'),
+        'type' => 'css',
+        'modifiers' => [
+          'theme-invert' => [
+            'title' => $this->t('Inverted colors'),
+            'bundles' => [
+              'paragraph' => [
+                'd_p_text_paged',
+                'd_p_single_text_block',
+                'd_p_group_of_text_blocks',
+              ]
+            ]
+          ],
+          'full-width' => [
+            'title' => $this->t('Full width'),
+            'bundles' => [
+              'paragraph' => [
+                'd_p_group_of_text_blocks'
+              ]
+            ]
+          ]
+        ]
+      ],
+      self::HEADING_TYPE_SETTING_NAME => [
+        'title' => $this->t('Heading type'),
+        'type' => 'select',
+        'options' => [
+          'h1' => $this->t('H1'),
+          'h2' => $this->t('H2'),
+          'h3' => $this->t('H3'),
+          'h4' => $this->t('H4'),
+          'h5' => $this->t('H5'),
+        ],
+        'default' => 'h2'
       ],
     ];
+  }
+
+  /**
+   * Is the modifier available in current bundle?
+   *
+   * @param array $modifier
+   *   The modifier definition.
+   *
+   * @return bool
+   *   Returns TRUE if modifier is in bundle.
+   */
+  protected function isModifierInBundle($modifier) {
+    $entity_type = $this->fieldDefinition->getTargetEntityTypeId();
+    $bundle = $this->fieldDefinition->getTargetBundle();
+    if (isset($modifier['bundles'][$entity_type])) {
+      return array_search($bundle, $modifier['bundles'][$entity_type]) !== FALSE;
+    }
+    return FALSE;
   }
 
   /**
@@ -45,16 +97,65 @@ class SettingsWidget extends WidgetBase {
       ],
     ];
 
-    $configOptions = $this->getConfigOptions();
-    foreach ($configOptions as $key => $options) {
-      $element[$key] = [
-        '#type' => 'textfield',
-        '#title' => $options['title'],
-        '#size' => 32,
-        '#default_value' => isset($config->$key) ? $config->$key : '',
-      ];
-      if ($element['#required']) {
-        $element[$key]['#required'] = TRUE;
+    $config_options = $this->getConfigOptions();
+    foreach ($config_options as $key => $options) {
+      $value = $config->$key ?? '';
+      switch ($options['type']) {
+        case 'css':
+          $classes = preg_split("/[\s,]+/", $value, -1, PREG_SPLIT_NO_EMPTY);
+          foreach ($options['modifiers'] as $class => $modifier) {
+            if (!$this->isModifierInBundle($modifier)) {
+              continue;
+            }
+            $class_key = array_search($class, $classes);
+            if ($class_key === FALSE) {
+              $default_value = 0;
+            }
+            else {
+              unset($classes[$class_key]);
+              $default_value = 1;
+            }
+            $element[$class] = [
+              '#type' => 'checkbox',
+              '#title' => $modifier['title'],
+              '#default_value' => $default_value,
+              '#attributes' => ['data-modifier' => $class],
+            ];
+          }
+
+          $element[$key] = [
+            '#type' => 'textfield',
+            '#title' => $options['title'],
+            '#size' => 32,
+            '#default_value' => join(' ', $classes),
+          ];
+          if ($element['#required']) {
+            $element[$key]['#required'] = TRUE;
+          }
+          break;
+
+        case 'select':
+          $element[$key] = [
+            '#type' => 'select',
+            '#title' => $options['title'],
+            '#options' => $options['options'],
+            '#default_value' => empty($value) ? $options['default'] : $value,
+          ];
+          if ($element['#required']) {
+            $element[$key]['#required'] = TRUE;
+          }
+          break;
+
+        default:
+          $element[$key] = [
+            '#type' => 'textfield',
+            '#title' => $options['title'],
+            '#size' => 32,
+            '#default_value' => $value,
+          ];
+          if ($element['#required']) {
+            $element[$key]['#required'] = TRUE;
+          }
       }
     }
     return ['value' => $element];
@@ -62,12 +163,26 @@ class SettingsWidget extends WidgetBase {
 
   /**
    * Validate the fields and convert them into a single value as json.
+   *
+   * {@inheritdoc}
    */
   public function validate($element, FormStateInterface $form_state) {
     $values = [];
-    $configOptions = $this->getConfigOptions();
-    foreach ($configOptions as $key => $option) {
-      $values[$key] = $element[$key]['#value'];
+    $config_options = $this->getConfigOptions();
+    foreach ($config_options as $key => $options) {
+      if ($options['type'] === 'css') {
+        $classes = preg_split("/[\s,]+/", $element[$key]['#value'], -1, PREG_SPLIT_NO_EMPTY);
+        foreach ($options['modifiers'] as $class => $modifier) {
+          if ($element[$class]['#value'] && $this->isModifierInBundle($modifier)) {
+            $classes[] = $class;
+          }
+        }
+        $classes = array_unique($classes);
+        $values[$key] = join(' ', $classes);
+      }
+      else {
+        $values[$key] = $element[$key]['#value'];
+      }
     }
     $form_state->setValueForElement($element, json_encode($values));
   }
