31ce90ed8aca7c8b56a326f89300f66e135f693c
Piotr Kamieniecki <piotr.kamieniecki@droptica.pl>
2019-12-23 17:06:08 +0100

    DD-256 Fixed classes logic for menu animations

--- themes/custom/droopler_theme/js/main-menu.js
+++ themes/custom/droopler_theme/js/main-menu.js
@@ -3,29 +3,23 @@
  * Main menu operation.
  */
 (function ($, Drupal) {
-
   "use strict";
-
   Drupal.behaviors.mainMenuMobileOperations = {
     attach: function (context, settings) {
       var $block = $('.we-mega-menu-submenu .block-block-content', context);
       var $titles = $('.field--name-field-d-main-title', $block);
-
       if ($titles.length) {
         var breakpointDesktop = 992;
         var blockContentClass = '.field--name-field-d-long-text';
-
-        $titles.once().click(function() {
+        $titles.once().click(function () {
           if (window.innerWidth < breakpointDesktop) {
             $(this).toggleClass('open').parent().find(blockContentClass).slideToggle();
-
             return false;
           }
         });
       }
     }
   };
-
   /**
    * Adds body tag class when mobile sidebar is opened.
    * Required for disabling body scroll and adding overlay.
@@ -34,16 +28,21 @@
    */
   Drupal.behaviors.mainMenuMobileNavbarListener = {
     attach: function (context, settings) {
-      $ ('#navbar-main button.navbar-toggler', context).click(function() {
+      $('#navbar-main button.navbar-toggler', context).click(function () {
         // Avoids classes toggle while collapsing.
-        if((!$('body').hasClass('navbar-open') && !$('.navbar').hasClass('collapsing')) || ($('.navbar').hasClass('show'))) {
-          $('body').toggleClass('navbar-open', !$(this).is('[aria-expanded="true"]'));
-          $('.navbar').toggleClass('open', !$(this).is('[aria-expanded="true"]'));
+        var aria = $(this).attr('aria-expanded');
+        if ((!$('body').hasClass('navbar-open') && !$('.navbar').hasClass('collapsing'))) {
+          $('body').toggleClass('navbar-open');
+          $('.navbar').toggleClass('open');
         }
+        else {
+          $('body').toggleClass('navbar-open');
+          $('.navbar').toggleClass('open');
+        }
+        $(this).attr('aria-expanded', (aria === 'false'));
       });
     }
   };
-
   /**
    * Adding toggle behavior for sidebar menu items having submenus.
    *
@@ -53,30 +52,24 @@
     attach: function (context, settings) {
       var $menuItems = $('.we-mega-menu-li.dropdown-menu', context);
       var $links = $('> a.we-mega-menu-li, > span.we-megamenu-nolink', $menuItems);
-
       if ($links.length) {
         var blockContentClass = '.we-mega-menu-submenu';
-
-        $links.each(function() {
+        $links.each(function () {
           var $thisLink = $(this);
           $thisLink.toggleClass('open', $thisLink.parent().is('.active'));
-
-          // The item can be <a> tag or just a <span> if no link available - thena the whole <span> is a toggler.
+          // The item can be <a> tag or just a <span> if no link available -
+          // thena the whole <span> is a toggler.
           var $toggler = $thisLink.is('a') ? $thisLink.find('.d-submenu-toggler') : $thisLink;
-
-          $toggler.once().click(function() {
+          $toggler.once().click(function () {
             var $linkItem = $(this);
             if ($linkItem.is('.d-submenu-toggler')) {
               $linkItem = $linkItem.parent();
             }
             $linkItem.toggleClass('open').next(blockContentClass).find('> .we-mega-menu-submenu-inner').slideToggle();
-
             return false;
           });
         });
       }
     }
   };
-
-
 })(jQuery, Drupal);
