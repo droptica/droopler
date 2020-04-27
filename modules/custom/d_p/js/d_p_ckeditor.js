726d786391d87e34d30dc6691f068e81b858ab18
Grzegorz Pietrzak <grzegorz.pietrzak@droptica.pl>
2020-04-27 16:13:46 +0200

    Changes for 8.2.0-rc2

--- modules/custom/d_p/js/d_p_ckeditor.js
+++ modules/custom/d_p/js/d_p_ckeditor.js
@@ -4,34 +4,8 @@
     attach: function (context, settings) {
 
       $(context).ready(function() {
-        d_p_ckeditor(true);
+        d_p_ckeditor();
       });
-
-      function d_p_ckeditor_geysir() {
-        if (typeof CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].element.$.parentElement.offsetParent.offsetParent.children['geysir-modal-form'] !== 'undefined') {
-          if (typeof CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document !== 'undefined') {
-            var doc = CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document.$;
-            d_p_ckeditor_add_js(doc);
-          } else {
-            setTimeout(d_p_ckeditor_geysir, 500);
-          }
-        }
-      }
-
-      function d_p_ckeditor_notgeysir() {
-        for (var instanceName in CKEDITOR.instances) {
-          var element = CKEDITOR.instances[instanceName].element.$.parentElement.offsetParent.parentElement.classList;
-          // check if it is group of text-block from block-text-paragraph
-          if (!(element.contains('paragraph-type--d-p-single-text-block'))) {
-            if (typeof CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document !== 'undefined') {
-              var doc = CKEDITOR.instances[Object.keys(CKEDITOR.instances)[0]].document.$; //get CKE doc!
-              d_p_ckeditor_add_js(doc);
-            } else {
-              setTimeout(d_p_ckeditor_notgeysir, 500);
-            }
-          }
-        }
-      }
 
       function d_p_ckeditor_add_js(doc) {
         var cssId = 'd_p_ckeditor';
@@ -47,15 +21,18 @@
         }
       }
 
-      function d_p_ckeditor(fade_in) {
-        fade_in = (fade_in == true) ? 500 : 0;
+      function d_p_ckeditor() {
         if (typeof CKEDITOR !== "undefined") {
-          // Check if it is a geysir paragraph editor
-          if (document.getElementById('geysir-modal')) {
-            d_p_ckeditor_geysir();
-          } else if (typeof CKEDITOR.instances !== "undefined") {
-            // For editor paragraph check every instance of ckeditor
-            d_p_ckeditor_notgeysir();
+          for (let instanceName in CKEDITOR.instances) {
+            if (CKEDITOR.instances[instanceName].element.$.parentElement.classList.contains('d_p_ckeditor_centered')) {
+              if (typeof CKEDITOR.instances[instanceName].document !== 'undefined') {
+                d_p_ckeditor_add_js(CKEDITOR.instances[instanceName].document.$);
+              } else {
+                setTimeout(function () {
+                  d_p_ckeditor_add_js(CKEDITOR.instances[instanceName].document.$);
+                }, 500);
+              }
+            }
           }
         } else {
           window.setTimeout(d_p_ckeditor, 500);
