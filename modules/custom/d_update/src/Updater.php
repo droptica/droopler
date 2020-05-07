67d5363d39e10fd650a8b335a637c5caa28f00fb
Mariusz Andrzejewski <mariusz.andrzejewski@droptica.pl>
2020-05-07 10:02:46 +0200

    DROOP-422 Log config modification aborting for not existing or newly created configs

--- modules/custom/d_update/src/Updater.php
+++ modules/custom/d_update/src/Updater.php
@@ -449,6 +449,8 @@ class Updater {
     $configData = $config->get();
 
     if ($config->isNew() || empty($configData)) {
+      $this->getLogger('d_update')
+        ->error("Unable to modify newly created or empty %config configuration. Aborting import", ['%config' => $configName]);
       return FALSE;
     }
 
