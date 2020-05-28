ab9f72a2f68143e44812fe885635f352508cc073
Piotr Kamieniecki <piotr.kamieniecki@droptica.pl>
2020-05-28 13:08:57 +0200

    DROOP-466 Modified override class to take into account auto label generation.

--- modules/custom/d_commerce/modules/d_commerce_product/src/Entity/DrooplerProductVariation.php
+++ modules/custom/d_commerce/modules/d_commerce_product/src/Entity/DrooplerProductVariation.php
@@ -3,6 +3,9 @@
 namespace Drupal\d_commerce_product\Entity;
 
 use Drupal\commerce_product\Entity\ProductVariation as CommerceProductVariation;
+use Drupal\Component\Plugin\Exception\PluginException;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Logger\LoggerChannelTrait;
 
 /**
  * Provides an override for commerce_product product variation entity class.
@@ -11,11 +14,42 @@ use Drupal\commerce_product\Entity\ProductVariation as CommerceProductVariation;
  */
 class DrooplerProductVariation extends CommerceProductVariation {
 
+  use LoggerChannelTrait;
+
   /**
    * {@inheritDoc}
    */
-  public function label() {
-    return sprintf("%s %s", $this->getProduct()->getTitle(), parent::label());
+  public function label(): string {
+    $variationType = $this->getVariationType();
+    if ($variationType && $variationType->shouldGenerateTitle()) {
+      $label = parent::label();
+    }
+    else {
+      $label = sprintf("%s %s", $this->getProduct()
+        ->getTitle(), parent::label());
+    }
+
+    return $label;
+  }
+
+  /**
+   * Returns variation type of current product variation.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface
+   *   Product variation type.
+   */
+  protected function getVariationType(): EntityInterface {
+    try {
+      $variationType = $this->entityTypeManager()
+        ->getStorage('commerce_product_variation_type')
+        ->load($this->bundle());
+    }
+    catch (PluginException $e) {
+      $this->getLogger('d_commerce_product')->error($e->getMessage());
+      $variationType = NULL;
+    }
+
+    return $variationType;
   }
 
 }
