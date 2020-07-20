7eb82370b103e8387723ddc1a4ed41d0a805797c
Radosław Stępiński <radoslaw.stepinski@droptica.pl>
2020-07-20 16:11:19 +0200

    DROOP-487 simplify code

--- modules/custom/d_product/src/Plugin/views/argument/NodeVid.php
+++ modules/custom/d_product/src/Plugin/views/argument/NodeVid.php
@@ -16,25 +16,10 @@ class NodeVid extends Vid {
    * Override the behavior of title(). Get the title of the revision.
    */
   public function titleQuery() {
-    $titles = [];
-
-    $results = $this->database->query('SELECT nr.vid, nr.nid, nfr.title 
+    return $this->database->query('SELECT nr.vid, nr.nid, nfr.title 
       FROM {node_revision} nr
       INNER JOIN {node_field_revision} nfr ON nr.vid = nfr.vid
-      WHERE nr.vid IN ( :vids[] )', array(':vids[]' => $this->value))->fetchAllAssoc('vid', \PDO::FETCH_ASSOC);
-    $nids = [];
-    foreach ($results as $result) {
-      $nids[] = $result['nid'];
-    }
-
-    $nodes = $this->nodeStorage->loadMultiple(array_unique($nids));
-
-    foreach ($results as $result) {
-      $nodes[$result['nid']]->set('title', $result['title']);
-      $titles[] = $nodes[$result['nid']]->label();
-    }
-
-    return $titles;
+      WHERE nr.vid IN ( :vids[] )', array(':vids[]' => $this->value))->fetchAllKeyed(1, 2);
   }
 
 }
