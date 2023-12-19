bf302238b61b0852424cc091fa8c345413c9522f
Damian Sikora <damian.sikora@droptica.pl>
2023-12-19 14:33:28 +0100

    DROOP-972 Restore 'centered ckeditor' functionlity for CKeditor 5 - improvements

--- modules/custom/d_p/js/d_p_ckeditor.js
+++ modules/custom/d_p/js/d_p_ckeditor.js
@@ -7,49 +7,69 @@
         d_p_ckeditor();
       });
 
-      // @todo: Rework this to better solution when it will be possible.
       function d_p_ckeditor() {
-        if (typeof Drupal.CKEditor5Instances !== "undefined") {
+        if (typeof Drupal.CKEditor5Instances !== 'undefined') {
           $(once('d_p_ckeditor_centered', '.d_p_ckeditor_centered', context)).each(function () {
-            let editor_id = $(this).find('textarea').attr('data-ckeditor5-id');
-            let editor = Drupal.CKEditor5Instances.get(editor_id);
-            let $editor_editable = $(this).find('.ck-editor__editable');
-            let $alignment_dropdown = $(this).find('.ck-toolbar-dropdown.ck-alignment-dropdown');
+            const editor_id = $(this).find('textarea').attr('data-ckeditor5-id');
+            const editor = Drupal.CKEditor5Instances.get(editor_id);
 
-            if (typeof editor === 'undefined' || !$editor_editable.length) {
+            if (typeof editor === 'undefined') {
               return;
             }
 
+            const model = editor.model;
+            const document = model.document;
+
             // Set default alignment to center.
-            $editor_editable.children().each(function () {
-              if (!$(this).hasClass('text-align-left') && !$(this).hasClass('text-align-right') && !$(this).hasClass('.text-align-justify')) {
-                $(this).addClass('text-align-center');
+            editor.execute('selectAll');
+            model.change(writer => {
+              const all_blocks = Array.from(document.selection.getSelectedBlocks()).filter(block => editor.commands.get('alignment')._canBeAligned(block));
+              for (const block of all_blocks) {
+                const current_alignment = block.getAttribute('alignment');
+                const element_classes = block.getAttribute('htmlPAttributes')?.classes ?? [];
+                const align_classes = ['text-align-left', 'text-align-right', 'text-align-justify'];
+                if (typeof current_alignment === 'undefined' && !align_classes.some(i => element_classes.includes(i))) {
+                  writer.setAttribute('alignment', 'center', block);
+                }
               }
             });
-            editor.setData($editor_editable.html());
 
-            // Add text-align-left class to selected element.
-            $(once('d_p_alignment_dropdown', $alignment_dropdown)).on('click', function() {
-              $(once('d_p_alignment_dropdown_item_btn', $alignment_dropdown.find('.ck.ck-toolbar__items button'))).on('click', function() {
-                let current_alignment = editor.commands.get('alignment').value;
-                if (current_alignment === 'left') {
+            // Add/remove text-align-left class to/from selected elements.
+            editor.commands.get('alignment').on('execute', (evt, args) => {
+              const alignment = args[0]?.value;
+              const current_focus = document.selection.focus;
+              const current_anchor = document.selection.anchor;
+              const current_first_range = document.selection.getFirstRange();
+              const current_selection_is_backward = document.selection.isBackward;
+              const selected_blocks = Array.from(document.selection.getSelectedBlocks()).filter(block => editor.commands.get('alignment')._canBeAligned(block));
+              let class_updated = false;
+
+              for (const block of selected_blocks) {
+                const block_view = editor.editing.mapper.toViewElement(block);
+                if (alignment === 'left') {
                   editor.editing.view.change(writer => {
-                    if (typeof editor?.editing?.view?.document?.selection?.focus?.parent?.parent !== 'undefined') {
-                      writer.addClass('text-align-left', editor.editing.view.document.selection.focus.parent.parent);
-                    }
+                    writer.addClass('text-align-left', block_view);
                   });
+                  class_updated = true;
                 }
-                else {
+                else if (block_view.hasClass('text-align-left')) {
                   editor.editing.view.change(writer => {
-                    if (typeof editor?.editing?.view?.document?.selection?.focus?.parent?.parent !== 'undefined') {
-                      writer.removeClass('text-align-left', editor.editing.view.document.selection.focus.parent.parent);
-                    }
+                    writer.removeClass('text-align-left', block_view);
                   });
+                  class_updated = true;
                 }
-                setTimeout(function() {
-                  editor.setData($editor_editable.html());
-                }, 100);
-              });
+              }
+
+              if (class_updated) {
+                // Save the updated data.
+                editor.data.set(editor.editing.view.getDomRoot().getInnerHTML(), {batchType: {isUndoable: true}});
+
+                // Restore the selection.
+                model.change(writer => {
+                  writer.setSelection(current_first_range, {backward: current_selection_is_backward});
+                  writer.setSelectionFocus(current_focus, current_anchor);
+                });
+              }
             });
           });
         } else {
