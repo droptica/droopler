50ac79f5a89d6d57bbc6ced806f78c03c2fdde5f
patryk-krzaczkowski-droptica <patryk.krzaczkowski@droptica.com>
2025-01-20 13:04:10 +0100

    move the droopler.info.yml to the project root and move it using the launch script.

--- launch-droopler-cms.sh
+++ launch-droopler-cms.sh
@@ -37,6 +37,9 @@ cp -r ddev_commands/* .ddev/commands/
 # Copy the starter theme to the project.
 cp -r starter-theme/ web/themes/custom/
 
+# Move droopler.info.yml to the project.
+mv droopler.info.yml web/profiles/droopler/droopler.info.yml
+
 ask_yes_no() {
     while true; do
         read -p "$1 [y/n]: " yn
