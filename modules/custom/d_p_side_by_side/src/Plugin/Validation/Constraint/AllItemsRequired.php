36199a67abd27f09e4adae989f764023f1d48f5c
Krystian <krystian.czach@droptica.com>
2025-11-14 10:27:55 +0100

    DRP: Compatibility with D11.2

--- modules/custom/d_p_side_by_side/src/Plugin/Validation/Constraint/AllItemsRequired.php
+++ modules/custom/d_p_side_by_side/src/Plugin/Validation/Constraint/AllItemsRequired.php
@@ -38,7 +38,7 @@ class AllItemsRequired extends Constraint {
   /**
    * {@inheritdoc}
    */
-  public function getRequiredOptions() {
+  public function getRequiredOptions(): array {
     return ['number', 'name'];
   }
 
