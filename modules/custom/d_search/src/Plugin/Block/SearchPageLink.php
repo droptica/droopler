23f1e542dea5876f275d899663516b2bae32e6fa
Krystian <krystian.czach@droptica.com>
2026-05-04 15:14:41 +0200

    DRP: Fix for new static

--- modules/custom/d_search/src/Plugin/Block/SearchPageLink.php
+++ modules/custom/d_search/src/Plugin/Block/SearchPageLink.php
@@ -47,6 +47,7 @@ class SearchPageLink extends BlockBase implements ContainerFactoryPluginInterfac
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
+    // @phpstan-ignore-next-line Drupal uses late static binding for plugin factory pattern.
     return new static(
       $configuration,
       $plugin_id,
